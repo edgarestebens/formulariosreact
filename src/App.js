e4dc869fc8419dc2f05d9ba8cc31770d53db446f
edgarestebens <colachoedgar@hotmail.com>
2019-01-12 12:32:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(){
+    super();
+    this.state={
+      name:"",
+      terms: true
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+
+            
+        
+          <input type="text" value={this.state.name} onChange={this.updateName.bind(this)}/>
+          <div>
+            <label>
+              <input type="checkbox" checked={this.state.terms} onClick={this.updateTerms.bind(this)}/> Acepto los términos
+            </label>
+          </div>
+          <button onClick={this.sayHi.bind(this)}>Say Hi!</button>
       </div>
     );
+  }
+
+
+  updateTerms(event){
+    this.setState({
+      terms:event.target.checked
+    });
+  }
+
+  updateName(event){
+    this.setState({
+      name:event.target.value
+    });
+  }
+
+  sayHi(){
+
+
+      if(this.state.terms){
+        alert('Hola ' + this.state.name)
+      }else{
+        alert("Debes aceptar los términos");
+      }      
   }
 }
 
